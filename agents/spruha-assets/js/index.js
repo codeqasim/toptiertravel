f9b14679e0ec4496c3f74c05b969bd15ac60c048
Malik Ahtisham <ahti.s6666@gmail.com>
2025-03-05 23:11:19 +0500

    Update index.js

--- agents/spruha-assets/js/index.js
+++ agents/spruha-assets/js/index.js
@@ -1,89 +1,89 @@
 /* project budget Chart */
-var options = {
-	series: [{
-		name: "Yearly Bookings",
-		data: window.permonthbookingcounts
-	}, {
-		// name: "Amount Used",
-		// data: [85, 65, 75, 38, 85, 35, 62, 40, 40, 64, 50, 89]
-	}],
-	chart: {
-		height: 320,
-		type: 'line',
-		zoom: {
-			enabled: false
-		},
-		dropShadow: {
-			enabled: true,
-			enabledOnSeries: undefined,
-			top: 5,
-			left: 0,
-			blur: 3,
-			color: '#000',
-			opacity: 0.1
-		},
-	},
-	dataLabels: {
-		enabled: false
-	},
-	legend: {
-		position: "top",
-		horizontalAlign: "center",
-		offsetX: -15,
-		fontWeight: "bold",
-	},
-	stroke: {
-		curve: 'smooth',
-		width: '3',
-		dashArray: [0, 5],
-	},
-	grid: {
-		borderColor: '#f2f6f7',
-	},
-	colors: ["rgb(98, 89, 202)", "rgba(98, 89, 202, 0.3)"],
-	yaxis: {
-		title: {
-			text: '',
-			style: {
-				color: '#adb5be',
-				// fontSize: '14px',
-				fontFamily: 'poppins, sans-serif',
-				fontWeight: 600,
-				cssClass: 'apexcharts-yaxis-label',
-			},
-		}
-	},
-	xaxis: {
-		type: 'month',
-		categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-		axisBorder: {
-			show: false,
-			color: 'rgba(119, 119, 142, 0.05)',
-			offsetX: 0,
-			offsetY: 0,
-		},
-		axisTicks: {
-			show: true,
-			borderType: 'solid',
-			color: 'rgba(119, 119, 142, 0.05)',
-			width: 6,
-			offsetX: 0,
-			offsetY: 0
-		},
-		labels: {
-			rotate: -90
-		}
-	}
-};
-document.getElementById('project').innerHTML = ''
-var chart2 = new ApexCharts(document.querySelector("#project"), options);
-chart2.render();
+// var options = {
+// // 	series: [{
+// // 		name: "Yearly Bookings",
+// // 		data: window.permonthbookingcounts
+// // 	}, {
+// // 		// name: "Amount Used",
+// // 		// data: [85, 65, 75, 38, 85, 35, 62, 40, 40, 64, 50, 89]
+// // 	}],
+// // 	chart: {
+// // 		height: 320,
+// // 		type: 'line',
+// // 		zoom: {
+// // 			enabled: false
+// // 		},
+// // 		dropShadow: {
+// // 			enabled: true,
+// // 			enabledOnSeries: undefined,
+// // 			top: 5,
+// // 			left: 0,
+// // 			blur: 3,
+// // 			color: '#000',
+// // 			opacity: 0.1
+// // 		},
+// // 	},
+// // 	dataLabels: {
+// // 		enabled: false
+// // 	},
+// // 	legend: {
+// // 		position: "top",
+// // 		horizontalAlign: "center",
+// // 		offsetX: -15,
+// // 		fontWeight: "bold",
+// // 	},
+// // 	stroke: {
+// // 		curve: 'smooth',
+// // 		width: '3',
+// // 		dashArray: [0, 5],
+// // 	},
+// // 	grid: {
+// // 		borderColor: '#f2f6f7',
+// // 	},
+// // 	colors: ["rgb(98, 89, 202)", "rgba(98, 89, 202, 0.3)"],
+// // 	yaxis: {
+// // 		title: {
+// // 			text: '',
+// // 			style: {
+// // 				color: '#adb5be',
+// // 				// fontSize: '14px',
+// // 				fontFamily: 'poppins, sans-serif',
+// // 				fontWeight: 600,
+// // 				cssClass: 'apexcharts-yaxis-label',
+// // 			},
+// // 		}
+// // 	},
+// // 	xaxis: {
+// // 		type: 'month',
+// // 		categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+// // 		axisBorder: {
+// // 			show: false,
+// // 			color: 'rgba(119, 119, 142, 0.05)',
+// // 			offsetX: 0,
+// // 			offsetY: 0,
+// // 		},
+// // 		axisTicks: {
+// // 			show: true,
+// // 			borderType: 'solid',
+// // 			color: 'rgba(119, 119, 142, 0.05)',
+// // 			width: 6,
+// // 			offsetX: 0,
+// // 			offsetY: 0
+// // 		},
+// // 		labels: {
+// // 			rotate: -90
+// // 		}
+// // 	}
+// // };
+// document.getElementById('project').innerHTML = ''
+// var chart2 = new ApexCharts(document.querySelector("#project"), options);
+// chart2.render();
 
-function project() {
-	chart2.updateOptions({
-		colors: ["rgb(" + myVarVal + ")", "rgba(" + myVarVal + ", 0.3)"],
-	})
-}
+// function project() {
+// 	chart2.updateOptions({
+// 		colors: ["rgb(" + myVarVal + ")", "rgba(" + myVarVal + ", 0.3)"],
+// 	})
+// }
 
 /* web design chart */
 // Function to get the last 6 months and current month
@@ -98,7 +98,7 @@ function getLast6Months() {
         // Go back to the previous month
         date.setMonth(date.getMonth() - 1);
     }
-    return months.reverse(); // Reverse to get the correct order (from oldest to newest)
+    return months.reverse(); 
 }
 
 // Get the categories for the x-axis
