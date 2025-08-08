fc3d3bb626e65129d40dece5a9fc52accc8bf5a2
FARAZ AHMAD <faraz@phptravels.org>
2025-08-08 12:52:06 +0500

    changed hotel booking saving api to old one cause of some database change pending in live

--- api/routes/Hotels.php
+++ api/routes/Hotels.php
@@ -848,12 +848,12 @@ $router->post('hotel_booking', function () {
     $db->insert("hotels_bookings", $param); //INSERTION OF BOOKING DATA INTO DATABASE
 
     // UPDATE PARTNER COMMISSION IF USER WHO IS MAKING THE BOOKING IS AN AGENT
-    if($user_type[0] == 'Agent'){
-        $partner = $db->select('partners' , '*' , ['child_id' => $_POST["user_id"]]);
-        if(isset($partner)){
-            $db->update('hotels_bookings',['partner_fee' => 10],['booking_ref_no' => $param['booking_ref_no']]);
-        }
-    }
+    // if($user_type[0] == 'Agent'){
+    //     $partner = $db->select('partners' , '*' , ['child_id' => $_POST["user_id"]]);
+    //     if(isset($partner)){
+    //         $db->update('hotels_bookings',['partner_fee' => 10],['booking_ref_no' => $param['booking_ref_no']]);
+    //     }
+    // }
 
     $data = (json_decode($_POST["user_data"]));
     $data = (object) array_merge((array) $data, array('booking_ref_no' => $param['booking_ref_no']));
