6eac45d4005f0b80175d798a57c8e3c0680a3d16
FARAZ AHMAD <faraz@phptravels.org>
2025-09-04 20:56:11 +0500

    testimonials and services removed bug

--- admin/our_services.php
+++ admin/our_services.php
@@ -1,5 +1,4 @@
 <?php 
-
 use Medoo\Medoo;
 
 require_once '_config.php';
@@ -7,62 +6,60 @@ auth_check();
 
 $title = T::our_services;
 include "_header.php";
-
 ?>
 
 <div class="page_head bg-transparent">
-<div class="panel-heading">
-<div class="float-start">
-<p class="m-0 page_title"><?=T::our_services?></p>
-</div>
-<div class="float-end">
-<!-- <a href="javascript:window.history.back();" data-toggle="tooltip" data-placement="top" title="Previous Page" class="loading_effect btn btn-warning">  Back</a> -->
-</div>
-</div>
+  <div class="panel-heading">
+    <div class="float-start">
+      <p class="m-0 page_title"><?=T::our_services?></p>
+    </div>
+    <div class="float-end"></div>
+  </div>
 </div>
 
 <div class="container mt-3">
-
 <?php 
 include('./xcrud/xcrud.php');
 $xcrud = Xcrud::get_instance();
 $xcrud->table('our_services');
 $xcrud->order_by('id','desc');
-$xcrud->columns('title,description,button_text,slug,background_image,created_at');
-$xcrud->column_class('background_image', 'zoom_img');
-$xcrud->change_type('background_image', 'image', true, array('width' => 200, 'path' => '../../uploads/'));
-// $xcrud->fields('status,name,country,rate,status');
-// $xcrud->validation_required('name');
-// $xcrud->validation_required('country');
-// $xcrud->validation_required('rate');
 
+/* Editable fields for Add/Edit */
+$xcrud->fields('title,description,button_text,slug,background_image');
+
+/* Columns in listing */
+$xcrud->columns('title,description,button_text,slug,background_image,created_at');
+
+/* Image upload setup */
+$xcrud->change_type('background_image', 'image', false, array(
+    'width' => 200,
+    'path'  => '../../uploads/'   // 👈 adjust if needed
+));
+
+/* UI cleanup */
 $xcrud->unset_title();
-// $xcrud->unset_csv();
 $xcrud->unset_view();
 
-// USER PERMISSIONS
-if (!isset($permission_delete)){ $xcrud->unset_remove(); }
+/* Permissions */
+if (!isset($permission_delete)){ 
+    $xcrud->unset_remove(); 
+}
 if (!isset($permission_edit)){ 
     $xcrud->unset_edit(); 
-   
 } else {
-
-    $xcrud->column_callback('status', 'create_status_icon');
-    $xcrud->field_callback('status','Enable_Disable');
-    $xcrud->column_callback('default', 'MakeDefault');
-    
+    // No status-related callbacks here 🚫
+    $xcrud->column_callback('default', 'MakeDefault'); // only if your table has a `default` column
 }
 
-$xcrud->column_width('name','200px');
-$xcrud->column_width('email','400px');
+/* Language */
 $xcrud->language($USER_SESSION->backend_user_language);
 
-// REFRESH PAGE
-$xcrud->after_insert('refresh');
-$xcrud->after_update('refresh');
+/* Remove undefined callbacks for now */
+// $xcrud->after_insert('refresh');
+// $xcrud->after_update('refresh');
 
 echo $xcrud->render();
-
 ?>
+</div>
 
 <?php include "_footer.php" ?>
--- admin/testimonials.php
+++ admin/testimonials.php
@@ -1,70 +1,72 @@
 <?php 
-
 use Medoo\Medoo;
 
 require_once '_config.php';
 auth_check();
 
-$title = T:: testimonials;
+$title = T::testimonials;
 include "_header.php";
-
 ?>
 
 <div class="page_head bg-transparent">
-<div class="panel-heading">
-<div class="float-start">
-<p class="m-0 page_title"><?=T::testimonials?></p>
-</div>
-<div class="float-end">
-<!-- <a href="javascript:window.history.back();" data-toggle="tooltip" data-placement="top" title="Previous Page" class="loading_effect btn btn-warning">  Back</a> -->
-</div>
-</div>
+  <div class="panel-heading">
+    <div class="float-start">
+      <p class="m-0 page_title"><?=T::testimonials?></p>
+    </div>
+    <div class="float-end"></div>
+  </div>
 </div>
 
 <div class="container mt-3">
-
 <?php 
 include('./xcrud/xcrud.php');
 $xcrud = Xcrud::get_instance();
 $xcrud->table('testimonials');
 $xcrud->order_by('id','desc');
-$xcrud->columns('name,profile_photo,ratings,title,description,photo,status,created_at');
-$xcrud->column_class('profile_photo', 'zoom_img');
-$xcrud->change_type('profile_photo', 'image', true, array('width' => 200, 'path' => '../../uploads/'));
-$xcrud->column_class('photo', 'zoom_img');
-$xcrud->change_type('photo', 'image', true, array('width' => 600, 'path' => '../../uploads/'));
-// $xcrud->fields('status,name,country,rate,status');
-// $xcrud->validation_required('name');
-// $xcrud->validation_required('country');
-// $xcrud->validation_required('rate');
 
+/* Show fields in form */
+$xcrud->fields('name,email,ratings,title,description,profile_photo,photo,status');
+
+/* Columns in grid */
+$xcrud->columns('name,profile_photo,ratings,title,description,photo,status,created_at');
+
+/* Images */
+$xcrud->change_type('profile_photo', 'image', false, array(
+    'width' => 200,
+    'path'  => '../../uploads/'   // 👈 Make sure this folder exists & is writable
+));
+$xcrud->change_type('photo', 'image', false, array(
+    'width' => 600,
+    'path'  => '../../uploads/'   // 👈 same here
+));
+
+/* Remove title */
 $xcrud->unset_title();
-// $xcrud->unset_csv();
 $xcrud->unset_view();
 
-// USER PERMISSIONS
-if (!isset($permission_delete)){ $xcrud->unset_remove(); }
+/* Permissions */
+if (!isset($permission_delete)){ 
+    $xcrud->unset_remove(); 
+}
 if (!isset($permission_edit)){ 
     $xcrud->unset_edit(); 
-   
 } else {
-
     $xcrud->column_callback('status', 'create_status_icon');
     $xcrud->field_callback('status','Enable_Disable');
     $xcrud->column_callback('default', 'MakeDefault');
-    
 }
 
+/* Layout tweaks */
 $xcrud->column_width('name','200px');
 $xcrud->column_width('email','400px');
 $xcrud->language($USER_SESSION->backend_user_language);
 
-// REFRESH PAGE
-$xcrud->after_insert('refresh');
-$xcrud->after_update('refresh');
+/* Remove undefined callbacks for now */
+// $xcrud->after_insert('refresh');
+// $xcrud->after_update('refresh');
 
 echo $xcrud->render();
-
 ?>
+</div>
 
 <?php include "_footer.php" ?>
