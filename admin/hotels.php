64d764e812aee0f86b820924b5213b467cee051a
FARAZ AHMAD <faraz@phptravels.org>
2025-09-17 17:03:10 +0500

    Updated xcrud room options made price mandatory for room option

--- admin/hotels.php
+++ admin/hotels.php
@@ -132,6 +132,8 @@ $rooms_options->change_type('childs','select','0','0,1,2,3,4,5,6,7,8,9');
 $rooms_options->change_type('quantity','select','0','1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17,18,19,20');
 $rooms_options->change_type('room_id','hidden');
 
+$rooms_options->validation_required('price');
+
 $seuser = DECODE($_SESSION['phptravels_backend_user']);
 $params = array("type_name"=> $seuser->backend_user_type);
 $role = GET('users_roles',$params);
