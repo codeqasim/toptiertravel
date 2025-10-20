de023d97340a0b4df1668ac48372d3cc5eed2529
FARAZ AHMAD <faraz@phptravels.org>
2025-10-20 20:25:13 +0500

    LOGOUT UPDATED

--- admin/logout.php
+++ admin/logout.php
@@ -1,6 +1,28 @@
 <?php
 session_start();
+
+// Destroy PHP session
 session_unset();
 session_destroy();
-header("location:Logins");
+
+// Expire JWT cookie
+$domain = ($_SERVER['HTTP_HOST'] === 'localhost:8888') ? 'localhost' : '.toptiertravel.vip';
+$secure = ($_SERVER['HTTP_HOST'] === 'localhost:8888') ? false : true;
+
+setcookie(
+    "admin_jwt",
+    "",
+    [
+        'expires' => time() - 3600, // expire in the past
+        'path' => '/',
+        'domain' => $domain,
+        'secure' => $secure,
+        'httponly' => true,
+        'samesite' => ($secure ? 'None' : 'Lax')
+    ]
+);
+
+// Redirect to login
+header("Location: login.php");
+exit;
 ?>
