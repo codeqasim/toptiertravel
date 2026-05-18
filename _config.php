7bc3e0dcfdd86c28855f02afff766d2f85460fa9
FARAZ AHMAD <faraz@phptravels.org>
2026-05-18 19:54:09 +0500

    php

--- _config.php
+++ _config.php
@@ -16,8 +16,12 @@ ini_set('display_errors', DEBUG ? 'On' : 'Off');
 define('api_key','api_key001');
 define('api_url', "api/"); // ADD SLASH IN THE END OF STRING
 define('api_modules', "https://api.phptravels.com"); // ADD Modules Path
+define('upload_path', __DIR__ . '/uploads/'); 
+define('upload_url', 'http://localhost:8888/toptiertravel/uploads/');
+define('user_upload_url', 'http://localhost:8888/toptiertravel/assets/uploads/');
+define("JWT_SECRET", "d9a3f7e81e2b4f20d4a87539ad7ccf46a8a5a9c04fbeb1f47e6e0f1adf382d12");
 
 define('server','localhost');
-define('dbname',"phptrave2_db");
-define('username',"phptrave2_db");
-define('password',"phptrave2_db");
+define('dbname',"iataco_top1");
+define('username',"root");
+define('password',"root");
